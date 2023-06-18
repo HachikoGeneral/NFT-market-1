56b23c87ca0f32aaac987fd64b17554eb8face7c
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-06-18 21:51:15 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x01f498193Ff29Ff8C654cdae5a3D5f446482c4fc"
-export const nftaddress = "0x4683f9e81835902EbB85C2342f96271b119B8934"
+export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
